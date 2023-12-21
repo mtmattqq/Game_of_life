ea76718f3d5276668da5d59cc70c08f445dd5655
mtmattqq <mtmattqq@gmail.com>
2023-12-21 21:40:04 +0800

    modify png from namespace to class

--- src/png.h
+++ src/png.h
@@ -5,14 +5,18 @@
 #include <cstdint>
 #include "board.h"
 
-namespace png {
-    struct chunk {
-        uint32_t length;
-        std::string ctype;
-        std::vector<unsigned char> data;
-        std::vector<unsigned char> crc32;
-    };
+struct chunk {
+    uint32_t length;
+    std::string ctype;
+    std::vector<unsigned char> data;
+    std::vector<unsigned char> crc32;
+};
 
+class png {
+private :
+    chunk header; 
+
+public :
     template<typename T> void gen(std::string, board<T>);
 };
 
